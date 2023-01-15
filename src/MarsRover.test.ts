c1c825e5f4e07f4147a7533ccf6104fb7bb3187d
lblake <treleven.lloyd@gmail.com>
2023-01-15 12:35:10 +0000

    worked on mars rover code

---
+++ src/MarsRover.test.ts
@@ -0,0 +1,134 @@
+import {Direction, MarsRover, ObstaclesInWorld, Point, Position, World} from './MarsRover'
+
+function aMarsRoverAt(x: number, y: number, directionRaw: string, world: World = World.unlimited()) {
+  return new MarsRover(Position.at(x, y).withinWorld(world).facing(directionRaw))
+}
+
+describe('Mars Rover', () => {
+  test(`cannot accept an invalid command`, () => {
+    const input = {x: 0, y: 0, direction: 'N'}
+    const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
+
+    expect(() => {
+      startingPositionRover.move('X')
+    }).toThrow("Invalid command 'X'")
+  })
+
+  describe.each([
+    {input: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'N'}},
+  ])('stays at the landing position', ({input, expected}) => {
+    test(`${input.x}, ${input.y} facing ${input.direction}`, () => {
+      const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
+      const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, expected.direction)
+
+      // no movement
+
+      expect(startingPositionRover).toEqual(expectedPositionRover)
+    })
+  })
+
+  describe.each([
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'f', expected: {x: 0, y: 1, direction: 'N'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'b', expected: {x: 0, y: -1, direction: 'N'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'l', expected: {x: 0, y: 0, direction: 'W'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'r', expected: {x: 0, y: 0, direction: 'E'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'ff', expected: {x: 0, y: 2, direction: 'N'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'fb', expected: {x: 0, y: 0, direction: 'N'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'lf', expected: {x: -1, y: 0, direction: 'W'}},
+    {input: {x: 0, y: 0, direction: 'S'}, commands: 'f', expected: {x: 0, y: -1, direction: 'S'}},
+  ])('moves after receiving commands', ({input, commands, expected}) => {
+    test(`'${commands}': ${input.x}, ${input.y} facing ${input.direction}`, () => {
+      const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
+      const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, expected.direction)
+
+      startingPositionRover.move(commands)
+
+      expect(startingPositionRover).toEqual(expectedPositionRover)
+    })
+  })
+
+  describe.each([
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'f', expected: {x: 0, y: 1}},
+    {input: {x: 0, y: 0, direction: 'S'}, commands: 'f', expected: {x: 0, y: -1}},
+    {input: {x: 0, y: 0, direction: 'E'}, commands: 'f', expected: {x: 1, y: 0}},
+    {input: {x: 0, y: 0, direction: 'W'}, commands: 'f', expected: {x: -1, y: 0}},
+
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'b', expected: {x: 0, y: -1}},
+    {input: {x: 0, y: 0, direction: 'S'}, commands: 'b', expected: {x: 0, y: 1}},
+    {input: {x: 0, y: 0, direction: 'E'}, commands: 'b', expected: {x: -1, y: 0}},
+    {input: {x: 0, y: 0, direction: 'W'}, commands: 'b', expected: {x: 1, y: 0}},
+  ])('advancing movements never affect direction', ({input, commands, expected}) => {
+    test(`Move '${commands}'. ${input.x}, ${input.y} facing ${input.direction}`, () => {
+      const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
+      const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, input.direction)
+
+      startingPositionRover.move(commands)
+
+      expect(startingPositionRover).toEqual(expectedPositionRover)
+    })
+  })
+
+  describe.each([
+    {input: 'N', expected: 'W'},
+    {input: 'W', expected: 'S'},
+    {input: 'S', expected: 'E'},
+    {input: 'E', expected: 'N'},
+  ])('Turn left', ({input, expected}) => {
+    test(`${input}`, () => {
+      const direction = new Direction(input)
+
+      const actual = direction.left()
+
+      expect(actual).toEqual(new Direction(expected))
+    })
+  })
+
+  describe.each([
+    {input: 'N', expected: 'E'},
+    {input: 'W', expected: 'N'},
+    {input: 'S', expected: 'W'},
+    {input: 'E', expected: 'S'},
+  ])('Turn Right', ({input, expected}) => {
+    test(`${input}`, () => {
+      const direction = new Direction(input)
+
+      const actual = direction.right()
+
+      expect(actual).toEqual(new Direction(expected))
+    })
+  })
+
+  describe.each([
+    {side: 'left', input: {x: 0, y: 0, direction: 'W'}, expected: {x: 15, y: 0}},
+    {side: 'right', input: {x: 15, y: 0, direction: 'E'}, expected: {x: 0, y: 0}},
+    {side: 'up', input: {x: 0, y: 15, direction: 'N'}, expected: {x: 0, y: 0}},
+    {side: 'down', input: {x: 0, y: 0, direction: 'S'}, expected: {x: 0, y: 15}},
+  ])('The world has limits', ({side, input, expected}) => {
+    test(`on the ${side}:`, () => {
+      const world = World.wrapping(16, 16)
+      const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction, world)
+      const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, input.direction, world)
+
+      startingPositionRover.move('f')
+
+      expect(startingPositionRover).toEqual(expectedPositionRover)
+    })
+  })
+
+  describe.each([
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'f', obstacles: [{x: 0, y: 1}], expected: {x: 0, y: 0, direction: 'N'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'ff', obstacles: [{x: 0, y: 2}], expected: {x: 0, y: 1, direction: 'N'}},
+    {input: {x: 0, y: 0, direction: 'N'}, commands: 'ffl', obstacles: [{x: 0, y: 2}], expected: {x: 0, y: 1, direction: 'W'}},
+  ])('The world has obstacles', ({input, commands, obstacles, expected}) => {
+    test(``, () => {
+      const obstaclesAsPoints = obstacles.map((o) => new Point(o.x, o.y))
+      const world = ObstaclesInWorld.with(World.unlimited(), obstaclesAsPoints)
+      const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction, world)
+      const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, expected.direction, world)
+
+      startingPositionRover.move(commands)
+
+      expect(startingPositionRover).toEqual(expectedPositionRover)
+    })
+  })
+})
---
+++ src/MarsRover.ts
@@ -0,0 +1,175 @@
+export class MarsRover {
+    private static map = {
+      f: (position: { forward: () => any }) => position.forward(),
+      b: (position: { backward: () => any }) => position.backward(),
+      l: (position: { turnLeft: () => any }) => position.turnLeft(),
+      r: (position: { turnRight: () => any }) => position.turnRight(),
+    }
+    private position: Position
+  
+    constructor(position: Position) {
+      this.position = position
+    }
+  
+    public move(commands: string) {
+      commands.split('').map((command) => {
+        try {
+          MarsRover.map[command](this.position)
+        } catch (e) {
+          throw Error(`Invalid command '${command}'`)
+        }
+      })
+    }
+  }
+  
+  export class Direction {
+    private static map = {
+      N: {left: 'W', right: 'E', forward: {x: 0, y: 1}, backward: {x: 0, y: -1}},
+      W: {left: 'S', right: 'N', forward: {x: -1, y: 0}, backward: {x: 1, y: 0}},
+      S: {left: 'E', right: 'W', forward: {x: 0, y: -1}, backward: {x: 0, y: 1}},
+      E: {left: 'N', right: 'S', forward: {x: 1, y: 0}, backward: {x: -1, y: 0}}
+    }
+    public facing: string
+  
+    constructor(facing: string) {
+      this.facing = facing
+    }
+  
+    public left() {
+      return new Direction(Direction.map[this.facing].left)
+    }
+  
+    public right() {
+      return new Direction(Direction.map[this.facing].right)
+    }
+  
+    public forward() {
+      return Direction.map[this.facing].forward
+    }
+  
+    public backward() {
+      return Direction.map[this.facing].backward
+    }
+  }
+  
+  export class Point {
+    public x: number
+    public y: number
+  
+    constructor(x: number, y: number) {
+      this.x = x
+      this.y = y
+    }
+  
+    public sum(other: Point) {
+      return new Point(this.x + other.x, this.y + other.y)
+    }
+  }
+  
+  export class Position {
+    public static at(x: number, y: number) {
+      return {
+        withinWorld(world: World) {
+          return {
+            facing(directionRaw: string) {
+              const direction = new Direction(directionRaw)
+              return new Position(x, y, direction, world)
+            }
+          }
+        }
+      }
+    }
+  
+    private direction: Direction
+    private point: Point
+    private world: World
+  
+    private constructor(x: number, y: number, direction: Direction, world: World) {
+      this.point = new Point(x, y)
+      this.direction = direction
+      this.world = world
+    }
+  
+    public turnLeft() {
+      this.direction = this.direction.left()
+    }
+  
+    public turnRight() {
+      this.direction = this.direction.right()
+    }
+  
+    public forward() {
+      this.sumVector(this.direction.forward())
+    }
+  
+    public backward() {
+      this.sumVector(this.direction.backward())
+    }
+  
+    private sumVector(vector: Point) {
+      this.point = this.world.move(this.point, vector)
+    }
+  
+  }
+  
+  export abstract class World {
+    public static unlimited(): World {
+      return new UnlimitedWorld()
+    }
+  
+    public static wrapping(width: number, height: number) {
+      return new WrappingWorld(width, height)
+    }
+  
+    public abstract move(origin: Point, vector: Point): Point
+  }
+  
+  export abstract class ObstaclesInWorld {
+    public static none(world: World): World {
+      return world
+    }
+  
+    public static with(world: World, obstacles: Point[]): World {
+      return new WorldWithObstacles(world, obstacles)
+    }
+  }
+  
+  class WorldWithObstacles implements World {
+    private obstacles: Point[]
+    private world: World
+  
+    constructor(world: World, obstacles: Point[]) {
+      this.world = world
+      this.obstacles = obstacles
+    }
+  
+    public move(origin: Point, vector: Point): Point {
+      const newValue = origin.sum(vector)
+      const obstacleFound = this.obstacles.some((e: Point) => e.x === newValue.x && e.y === newValue.y)
+      if (obstacleFound) {
+        return origin
+      }
+      return this.world.move(origin, vector)
+    }
+  }
+  
+  class WrappingWorld implements World {
+    private height: number
+    private width: number
+  
+    constructor(width: number, height: number) {
+      this.height = height
+      this.width = width
+    }
+  
+    public move(origin: Point, vector: Point): Point {
+      const point = origin.sum(vector)
+      return new Point((point.x + this.width) % this.width, (point.y + this.height) % this.height)
+    }
+  }
+  
+  class UnlimitedWorld implements World {
+    public move(origin: Point, vector: Point): Point {
+      return origin.sum(vector)
+    }
+  }
